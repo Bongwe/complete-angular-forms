b3abf597bc110bddbf875a078e08c025aecc5c48
Mzwandile Mahlangu <mzwandile.mahlangu@entelect.co.za>
2020-03-22 21:38:09 +0200

    setting up for item edit

--- src/app/shopping-list/shopping-edit/shopping-edit.component.ts
+++ src/app/shopping-list/shopping-edit/shopping-edit.component.ts
@@ -1,6 +1,9 @@
+import { Subscription } from 'rxjs/Subscription';
 import {
   Component,
-  OnInit
+  OnInit,
+  OnDestroy
+
 } from '@angular/core';
 
 import { Ingredient } from '../../shared/ingredient.model';
@@ -12,12 +15,24 @@ import { NgForm } from '@angular/forms';
   templateUrl: './shopping-edit.component.html',
   styleUrls: ['./shopping-edit.component.css']
 })
-export class ShoppingEditComponent implements OnInit {
-
+export class ShoppingEditComponent implements OnInit, OnDestroy {
+  subscription:Subscription;
+  editMode = false;
+  editedItemIndex: number;
   constructor(private slService: ShoppingListService) { }
 
   ngOnInit() {
+   this.subscription = this.slService.startedEditing
+    .subscribe((index: number)=>{
+      this.editedItemIndex = index;
+      this.editMode = true;
+    });
   }
+
+
+  ngOnDestroy() {
+    this.subscription.unsubscribe();
+   }
 
   onAddItem(form: NgForm) {
     const valaue = form.value;
